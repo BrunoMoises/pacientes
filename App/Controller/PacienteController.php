5766229265e92885a9e85df3eb60312d99dae2c1
Bruno <bruno_moises_stz@hotmail.com>
2023-02-06 19:45:51 -0300

    Testando se autoload esta funcionando e acrescentando .htaccess a Public/api para redirecionar sempre para index.php

---
+++ App/Controller/PacienteController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Controller;
+
+class PacienteController
+{
+    function __construct(){
+        echo "CHAMOU A PACIENTECONTROLLER";
+    }
+}
---
+++ Public/api/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once("../../vendor/autoload.php");
+use App\Controller\PacienteController;
+
+$pacienteController = new PacienteController();
+?>
