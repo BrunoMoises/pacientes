157ad0c1fbd83b800067673d4ffaf56d0425b22d
Bruno <bruno_moises_stz@hotmail.com>
2023-02-10 07:40:55 -0300

    Reformulando model de pacientes deixando o codigo mais limpo e otimizado

--- App/Model/PacienteModel.php
+++ App/Model/PacienteModel.php
@@ -4,7 +4,6 @@ namespace App\Model;
 
 use App\Entity\Paciente;
 use App\Database\Database;
-use Exception;
 use mysqli;
 
 class PacienteModel
@@ -15,108 +14,85 @@ class PacienteModel
 
     public function __construct()
     {
+        $this->db = new mysqli('localhost', 'root', '', 'cadastro');
+        $this->items = new Database($this->db);
         $this->load();
     }
 
-    public function getConnection()
-    {
-        $this->db = null;
-        try {
-            $this->db = new mysqli('localhost', 'root', '', 'cadastro');
-        } catch (Exception $e) {
-            echo "Database could not be connected: " . $e->getMessage();
-        }
-
-        return $this->db;
-    }
-
-    public function readAll()
+    public function readAll(): array
     {
         return $this->listPaciente;
     }
 
-    public function readById($id)
+    public function readById(int $id)
     {
-        $this->db = $this->getConnection();
-        $this->items = new Database($this->db);
-
         $this->items->id = $id;
         $this->items->getId();
 
-        if ($this->items->nome != null) {
-
-            $pac_arr = array(
-                "id" => $this->items->id,
-                "nome" => $this->items->nome,
-                "cpf" => $this->items->cpf
-            );
-
-            http_response_code(200);
-
-            return json_encode($pac_arr);
+        if ($this->items->nome === null) {
+            http_response_code(404);
+            return json_encode(array("message" => "Não encontrado."));
         }
+
+        $pac_arr = array(
+            "id" => $this->items->id,
+            "nome" => $this->items->nome,
+            "cpf" => $this->items->cpf
+        );
+
+        http_response_code(200);
+
+        return json_encode($pac_arr);
     }
 
     public function create(Paciente $paciente)
     {
-        $this->db = $this->getConnection();
-        $this->items = new Database($this->db);
-
         $this->items->nome = $paciente->getNome();
         $this->items->cpf = $paciente->getCpf();
 
-        if (!$this->items->createPaciente())
+        if (!$this->items->createPaciente()) {
             return "Erro ao gravar";
+        }
 
         return "ok";
     }
 
-
     public function update(Paciente $paciente)
     {
-        $this->db = $this->getConnection();
-        $this->items = new Database($this->db);
-
         $this->items->id = $paciente->getId();
         $this->items->nome = $paciente->getNome();
         $this->items->cpf = $paciente->getCpf();
 
-        if (!$this->items->updatePaciente())
+        if (!$this->items->updatePaciente()) {
             return "Erro ao gravar";
+        }
 
         return "ok";
     }
 
-    public function delete($id)
+    public function delete(int $id)
     {
-        $this->db = $this->getConnection();
-        $this->items = new Database($this->db);
+        $this->items->id = $id;
 
-        $this->items->id = isset($id) ? $id : die();
-
-        if (!$this->items->deletePaciente())
+        if (!$this->items->deletePaciente()) {
             return "Erro ao gravar";
+        }
 
         return "ok";
     }
 
     private function load()
     {
-        $this->db = $this->getConnection();
-        $this->items = new Database($this->db);
-
         $records = $this->items->getPacientes();
         $itemCount = $records->num_rows;
 
-        if ($itemCount > 0) {
-            while ($row = $records->fetch_assoc()) {
-                array_push($this->listPaciente, $row);
-            }
-        } else {
+        if ($itemCount === 0) {
             http_response_code(404);
-            return json_encode(
-                array("message" => "Não encontrado.")
-            );
+            return json_encode(array("message" => "Não encontrado."));
+        }
+
+        while ($row = $records->fetch_assoc()) {
+            array_push($this->listPaciente, $row);
         }
     }
 }
