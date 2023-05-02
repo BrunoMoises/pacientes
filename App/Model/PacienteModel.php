1d9b010c3d3a2a6bbde0bd6bea77f863fec3c75e
Bruno <bruno_moises_stz@hotmail.com>
2023-05-02 16:21:24 -0300

    refactor: levando queries para a model

--- App/Model/PacienteModel.php
+++ App/Model/PacienteModel.php
@@ -2,17 +2,23 @@
 
 namespace App\Model;
 
+use App\Database\Connection;
 use App\Entity\Paciente;
 use App\Database\Database;
+use Exception;
+use PDO;
 
 class PacienteModel
 {
-    private Database $items;
+    private PDO $conn;
+    private Connection $obj;
+    private string $db_table = "paciente";
     private array $listPaciente = [];
 
     public function __construct()
     {
-        $this->items = new Database();
+        $this->obj = new Connection;
+        $this->conn = $this->obj->connect();
         $this->load();
     }
 
@@ -23,69 +29,107 @@ class PacienteModel
 
     public function readById(int $id): false|string
     {
-        $this->items->id = $id;
-        $this->items->getId();
+        $sqlQuery = "SELECT id, nome, cpf FROM " . $this->db_table . " WHERE id = ? ";
+        $record = $this->conn->prepare($sqlQuery);
+        $record->execute(array($id));
+        $dataRow = $record->fetch();
 
-        if ($this->items->nome === null) {
+        if (!$dataRow) {
             http_response_code(404);
-            return json_encode(array("message" => "Não encontrado."));
+            return json_encode(array("result" => "Não encontrado."));
         }
 
         $pac_arr = array(
-            "id" => $this->items->id,
-            "nome" => $this->items->nome,
-            "cpf" => $this->items->cpf
+            "id" => $id,
+            "nome" => $dataRow['nome'],
+            "cpf" => $dataRow['cpf']
         );
 
         http_response_code(200);
-
         return json_encode($pac_arr);
     }
 
-    public function create(Paciente $paciente): string
+    public function create(Paciente $paciente)
     {
-        $this->items->nome = $paciente->getNome();
-        $this->items->cpf = $paciente->getCpf();
+        $values = [
+            $paciente->getNome(),
+            $paciente->getCpf()
+        ];
+        $insertQuery = "INSERT INTO cadastro.paciente (id, nome, cpf) VALUES (NULL,?,?);";
+        $selectQuery = "SELECT * FROM cadastro.paciente WHERE id = LAST_INSERT_ID();";
 
-        if (!$this->items->createPaciente()) {
-            return "Erro ao gravar";
+        $insertRecord = $this->conn->prepare($insertQuery);
+        $selectRecord = $this->conn->prepare($selectQuery);
+
+        try {
+            $this->conn->beginTransaction();
+            $insertRecord->execute($values);
+            $selectRecord->execute();
+            $this->conn->commit();
+
+            $result = $selectRecord->fetch(PDO::FETCH_ASSOC);
+            http_response_code(201);
+            return json_encode($result);
+        } catch (Exception $e) {
+            $this->conn->rollback();
+            http_response_code(400);
+            return json_encode(array("result" => "Não foi possível a criação."));
         }
-
-        return "ok";
     }
 
     public function update(Paciente $paciente): string
     {
-        $this->items->id = $paciente->getId();
-        $this->items->nome = $paciente->getNome();
-        $this->items->cpf = $paciente->getCpf();
+        $values = [
+            $paciente->getNome(),
+            $paciente->getCpf(),
+            $paciente->getId()
+        ];
+        $sqlQuery = "UPDATE " . $this->db_table . " SET nome = ?, cpf = ? WHERE id = ?";
+        $selectQuery = "SELECT * FROM " . $this->db_table . " WHERE id = ?";
 
-        if (!$this->items->updatePaciente()) {
-            return "Erro ao gravar";
+        $updateRecord = $this->conn->prepare($sqlQuery);
+        $selectRecord = $this->conn->prepare($selectQuery);
+
+        try {
+            $this->conn->beginTransaction();
+            $updateRecord->execute($values);
+            $selectRecord->execute(array($paciente->getId()));
+            $this->conn->commit();
+
+            $result = $selectRecord->fetch(PDO::FETCH_ASSOC);
+            http_response_code(200);
+            return json_encode($result);
+        } catch (Exception $e) {
+            $this->conn->rollback();
+            http_response_code($e->getCode());
+            return json_encode(array("result" => "Não foi possível a alteração."));
         }
-
-        return "ok";
     }
 
     public function delete(int $id): string
     {
-        $this->items->id = $id;
-
-        if (!$this->items->deletePaciente()) {
-            return "Erro ao gravar";
+        $sqlQuery = "DELETE FROM " . $this->db_table . " WHERE id = ?";
+        $record = $this->conn->prepare($sqlQuery);
+        try {
+            $this->conn->beginTransaction();
+            $record->execute([$id]);
+            $this->conn->commit();
+            http_response_code(201);
+            return json_encode(array("result" => "OK"));
+        } catch (Exception $e) {
+            $this->conn->rollback();
+            http_response_code($e->getCode());
+            return json_encode(array("result" => "Não foi possivel a alteração."));
         }
-
-        return "ok";
     }
 
     private function load(): void
     {
-        $records = $this->items->getPacientes();
-        $itemCount = $records->rowCount();
+        $records = $this->conn->query('SELECT id, nome, cpf FROM ' . $this->db_table);
 
-        if ($itemCount === 0) {
-            http_response_code(404);
-            json_encode(array("message" => "Não encontrado."));
+        if (!$records) {
+            http_response_code($records->errorCode());
+            json_encode(array("result" => "Não encontrado."));
             return;
         }
 
